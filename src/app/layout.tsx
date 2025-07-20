48479cf37bb8a6f4d16dddccf458502e0a501176
Abhishek Albela <abhishek242005@gmail.com>
2025-07-20 15:37:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,18 +22,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3781633352100587" crossOrigin="anonymous"></script>
-        {/* Tag for child-directed treatment */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (adsbygoogle = window.adsbygoogle || []).push({
-                google_ad_client: "ca-pub-3781633352100587",
-                enable_page_level_ads: true,
-                tag_for_child_directed_treatment: 1
-              });
-            `,
-          }}
-        />
       </head>
       <body className="font-body antialiased" suppressHydrationWarning={true}>
         <InterstitialWrapper>
