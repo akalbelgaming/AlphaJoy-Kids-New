8e875bc6fd41649d5ce2a2f5c184e9535f3127d9
Abhishek Albela <abhishek242005@gmail.com>
2025-07-25 09:02:06 +0000

    ok to chale laga do adsance bala ads browsers bala page me baki kisi me

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-        {/* AdSense script removed as per user request to switch to AdMob */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3781633352100587"
+     crossOrigin="anonymous"></script>
       </head>
       <body className="font-body antialiased" suppressHydrationWarning={true}>
         <InterstitialWrapper>
