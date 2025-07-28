030daed0592fa77825c3f608008dd11afaba1a5a
Abhishek Albela <abhishek242005@gmail.com>
2025-07-28 12:12:20 +0000

    PS C:\Users\akalb\Desktop\AlphaJoy-Kids-New> git reset --hard origin/mas

---
+++ C:\Users\akalb\Desktop\AlphaJoy-Kids-New\next.config.ts
@@ -0,0 +1,36 @@
+
+import type {NextConfig} from 'next';
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  // add your own icons to src/app/manifest.json
+  // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
+});
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+// Temporarily disabling PWA to ensure `output: 'export'` works correctly.
+export default withPWA(nextConfig);
+// export default nextConfig;
